0511c5d01ef8befb8df6778ad22031679faef27f
bruderj15 <julian.bruder@outlook.com>
2025-01-17 22:56:39 +0100

    main: naming hygiene

--- app/PaymentService.hs
+++ app/PaymentService.hs
@@ -8,23 +8,23 @@ import Data.UUID (toString)
 import GHC.Generics (Generic)
 
 data OrderCreatedEvent = OrderCreatedEvent
-  { oId     :: String
+  { orderId :: String
   , userId  :: String
   , amount  :: Double
   } deriving stock (Show, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
 data Payment = Payment
-  { id        :: String
-  , orderId   :: String
-  , success   :: Bool
+  { id             :: String
+  , paymentOrderId :: String
+  , success        :: Bool
   } deriving stock (Show, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
 orderCreatedEventHandler :: OrderCreatedEvent -> IO ()
 orderCreatedEventHandler event = do
   uId <- nextRandom
-  let payment = Payment { id = toString uId, orderId = oId event, success = True }
+  let payment = Payment { id = toString uId, paymentOrderId = orderId event, success = True }
   -- imagine here that payment always succeeds and we serialize it successfully
   -- imagine further serialization into e.g. Amazon RDS which would trigger a PaymentProcessedEvent
   print payment
